9af1eda76742fc7f0f4fcd2d842be70af6195a54
Nathan Barlow <nathanjbarlow@gmail.com>
2023-07-21 06:40:22 -0500

    Start using brain for event loop

--- src/clojure_amortizor/core.clj
+++ src/clojure_amortizor/core.clj
@@ -1,6 +1,21 @@
-(ns clojure-amortizor.core)
+(ns clojure-amortizor.core
+  (:require [nixnomad.brain.core :refer [spark]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn run-handler [{:keys [run-handler data-bag]}]
+  (let [{:keys [fps counter counter-print-step]} data-bag]
+    (cond
+      (> counter 1000)
+      [false {:run-handler run-handler :data-bag (merge data-bag {:counter (inc counter)})}]
+      (zero? (mod counter counter-print-step))
+      (do
+        (println (str "FPS: " fps))
+        [true {:run-handler run-handler :data-bag (merge data-bag {:counter (inc counter)})}])
+      :else
+      [true {:run-handler run-handler :data-bag (merge data-bag {:counter (inc counter)})}])))
+
+(defn -main
+  "Example application copied from brain using brain as a library"
+  [& _args]
+
+  (spark {:run-handler run-handler :data-bag {:counter 0 :counter-print-step 1}}))
--- test/clojure_amortizor/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-amortizor.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-amortizor.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
