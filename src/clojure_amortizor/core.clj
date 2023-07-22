002221a1c6f75e52f84561e714c5cf5be2ff36e6
soniccyclone <nathanjbarlow@gmail.com>
2023-07-21 22:19:41 -0500

    Create loan

--- src/clojure_amortizor/core.clj
+++ src/clojure_amortizor/core.clj
@@ -1,5 +1,6 @@
 (ns clojure-amortizor.core
-  (:require [nixnomad.brain.core :refer [spark]])
+  (:require [nixnomad.brain.core :refer [spark]]
+            [clojure-amortizor.loan :refer [create-loan calculate-daily-interest get-accrued-interest]])
   (:gen-class))
 
 (defn run-handler [{:keys [run-handler data-bag]}]
@@ -19,3 +20,11 @@
   [& _args]
 
   (spark {:run-handler run-handler :data-bag {:counter 0 :counter-print-step 1}}))
+
+
+(comment
+  (let [loan (create-loan "test-loan" 2.5 100 0 5)]
+    (->> loan
+         calculate-daily-interest
+         (get-accrued-interest 30)))
+  :rcf)
---
+++ src/clojure_amortizor/loan.clj
@@ -0,0 +1,19 @@
+(ns clojure-amortizor.loan)
+
+(defn create-loan
+  "Creates an active loan"
+  [name interest-rate principal-balance accrued-interest minimum-monthly-payment]
+  {:name name
+   :interest-rate interest-rate
+   :principal-balance principal-balance
+   :accrued-interest accrued-interest
+   :minimum-monthly-payment minimum-monthly-payment
+   :loan-state :active})
+
+(defn calculate-daily-interest
+  [{:keys [interest-rate principal-balance] :as loan}]
+  (/ (* principal-balance interest-rate) 365.25))
+
+(defn get-accrued-interest
+  [days daily-interest]
+  (* days daily-interest))
